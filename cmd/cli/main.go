ff5cd45da67fbe200397365086d3049a80497183
Vladislav Atakhanov <eloh1mzee@gmail.com>
2025-10-08 01:53:51 +0500

    refactor: move cmd/cli.go -> cmd/cli/main.go

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -2,12 +2,11 @@ package main
 
 import (
 	"fmt"
-	"github.com/vladislav-atakhanov/pswd/cmd/cli"
 	"os"
 )
 
 func main() {
-	if err := cli.Execute(); err != nil {
+	if err := Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
