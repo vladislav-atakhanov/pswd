ae76647b9e36ad6deb3f24f706614ab09b6ef20e
Vladislav Atakhanov <eloh1mzee@gmail.com>
2026-07-01 14:34:10 +0500

    feat(cli): add init

--- cmd/cli/add_device.go
+++ cmd/cli/add_device.go
@@ -8,45 +8,45 @@ import (
 	"github.com/spf13/cobra"
 )
 
+func parsePublicKey(token string) (string, [32]byte, error) {
+
+	tokenRaw, err := base64.URLEncoding.DecodeString(token)
+	if err != nil {
+		return "", [32]byte{}, fmt.Errorf("decode token: %w", err)
+	}
+	if len(tokenRaw) < 34 {
+		return "", [32]byte{}, fmt.Errorf("invalid token length: got %d bytes, expected at least 34+", len(tokenRaw))
+	}
+
+	var pub [32]byte
+	copy(pub[:], tokenRaw)
+
+	var name string
+	if len(tokenRaw) > 32 {
+		nameLen := binary.BigEndian.Uint16(tokenRaw[32:34])
+		name = string(tokenRaw[34 : 34+nameLen])
+	}
+	return name, pub, nil
+}
+
 var addDeviceCmd = &cobra.Command{
 	Use:   "add-device <token>",
 	Short: "Add a device to the vault",
 	Long:  "Add a new device to the vault. Token can be a raw base64-URL-encoded public key (32 bytes) or the full output of pswd export.",
 	Args:  cobra.ExactArgs(1),
 	RunE: withVault(func(ctx *vaultContext, args []string) error {
-		token := args[0]
-
-		tokenRaw, err := base64.URLEncoding.DecodeString(token)
+		name, pub, err := parsePublicKey(args[0])
 		if err != nil {
-			return fmt.Errorf("decode token: %w", err)
+			return err
 		}
-		if len(tokenRaw) < 32 {
-			return fmt.Errorf("invalid token length: got %d bytes, expected at least 32", len(tokenRaw))
-		}
-
-		var pub [32]byte
-		copy(pub[:], tokenRaw)
-
-		var deviceName string
-		if len(tokenRaw) > 32 {
-			nameLen := binary.BigEndian.Uint16(tokenRaw[32:34])
-			deviceName = string(tokenRaw[34 : 34+nameLen])
-		} else {
-			nameBytes, err := readPassword("Enter device name: ")
-			if err != nil {
-				return fmt.Errorf("read device name: %w", err)
-			}
-			deviceName = string(nameBytes)
-		}
-
-		if err := ctx.Vault.AddDevice(pub, deviceName, ctx.File, ctx.Priv); err != nil {
+		if err := ctx.Vault.AddDevice(pub, name, ctx.File, ctx.Priv); err != nil {
 			return fmt.Errorf("add device: %w", err)
 		}
 		if err := ctx.Vault.Save(ctx.File); err != nil {
 			return fmt.Errorf("save vault: %w", err)
 		}
 
-		fmt.Println("Device", deviceName, "added to vault")
+		fmt.Println("Device", name, "added to vault")
 		return nil
 	}),
 }
---
+++ cmd/cli/init.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/vladislav-atakhanov/pswd/internal/vault"
+)
+
+var initCmd = &cobra.Command{
+	Use:   "init <name> <public-key>",
+	Short: "Display vault contents",
+	Args:  cobra.ExactArgs(2),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		name, pub, err := parsePublicKey(args[1])
+		if err != nil {
+			return err
+		}
+		file, err := os.Create(args[0])
+		if err != nil {
+			return fmt.Errorf("open vault: %w", err)
+		}
+		defer file.Close()
+		v := vault.New(pub, name)
+		return v.Save(file)
+	},
+}
